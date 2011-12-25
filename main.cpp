68bbbb58821f952ce7c08d0d72bfa25150ec05da
Nicholas 'Indy' Ray <arelius@gmail.com>
2011-12-24 19:50:17 -0800

    Windows system implementation.
    * Win32 Window creation.
    * Message loop.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,24 @@
 // See the LICENSE file for usage, modification, and distribution terms.
 int InitWindowAndLoop(int argc, char** argv);
 
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+// argc and __argv in windows.
+#include <stdlib.h>
+extern HINSTANCE WinSys_hInstance;
+#endif
+
+#ifdef _WIN32
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
 int main(int argc, char** argv)
+#endif
 {
+#ifdef _WIN32
+    WinSys_hInstance = hInstance;
+    int argc = __argc;
+    char** argv = __argv;
+#endif
     return InitWindowAndLoop(argc, argv);
 }
--- sys_win.cpp
+++ sys_win.cpp
@@ -5,8 +5,51 @@
 #include "view_decl.h"
 #include "gl_all.h"
 
+HINSTANCE WinSys_hInstance;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 int InitWindowAndLoop(int argc, char** argv)
 {
+    char* ClassName = "BOOTEDWIN";
+    WNDCLASSEX ClassStruct;
+    memset(&ClassStruct, 0, sizeof(ClassStruct));
+    ClassStruct.cbSize = sizeof(ClassStruct);
+    ClassStruct.lpfnWndProc = WndProc;
+    ClassStruct.hInstance = WinSys_hInstance;
+    ClassStruct.lpszClassName = ClassName;
+    RegisterClassEx(&ClassStruct);
+    HWND hWnd = CreateWindow(ClassName,
+                             "BootEd Window.",
+                             WS_TILEDWINDOW,
+                             CW_USEDEFAULT,
+                             CW_USEDEFAULT,
+                             1024,
+                             768,
+                             NULL,
+                             NULL,
+                             WinSys_hInstance,
+                             NULL);
+
+    ShowWindow(hWnd, SW_SHOW);
+
+    // This message loop is meant to consume all system resources, needs
+    // to be changed for apps that are meant to idle, I.E. editors.
+    bool continue_loop = true;
+    while(continue_loop)
+    {
+        MSG msg;
+        while(PeekMessage(&msg, NULL, 0, 0, 0))
+        {
+            if(!GetMessage(&msg, NULL, 0, 0))
+                continue_loop = false;
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return 0;
     void InitGLExt();
     InitGLExt();
 
@@ -14,6 +57,22 @@ int InitWindowAndLoop(int argc, char** argv)
     ResizeView(view, 0, 0);
     UpdateView(view);
     FinishView(view);
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+    case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
     return 0;
 }
 
