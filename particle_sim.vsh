b6c50df5691dea7aaa88aa45c871386bd2f55c12
Nicholas "Indy" Ray <Arelius@gmail.com>
2013-11-05 20:25:29 -0800

    Initital transform feedback simulation.
    * Using transform feedback double buffering for particle sim.
    * Added test noise function for particle spawning.

--- particle_sim.vsh
+++ particle_sim.vsh
@@ -1,7 +1,10 @@
 // Copyright (c) 2013, Nicholas "Indy" Ray. All rights reserved.
 // See the LICENSE file for usage, modification, and distribution terms.
 
+varying vec4 out_Position;
+
 void main(void)
 {
-    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+  out_Position = gl_Vertex;
+  gl_Position = out_Position;
 }
--- source/particles.cpp
+++ source/particles.cpp
@@ -40,9 +40,10 @@ VertexDef GenParticleVertDef()
 
 struct Particles
 {
+    static const unsigned int BUFFER_COUNT = 2;
     unsigned int vertex_count;
-    GLuint vertex_buffer;
-    GLuint vertex_texture;
+    GLuint vertex_buffers[BUFFER_COUNT];
+    GLuint vertex_textures[BUFFER_COUNT];
     GLuint atomic_count_buffer;
 
     GLuint shader_gen;
@@ -59,7 +60,7 @@ struct Particles
     void CaptureUniforms()
     {
         int i = 0;
-        gen_uniforms.add_uniform("particle_buffer", &vertex_texture, Uniform_Image, i++, shader_gen);
+        gen_uniforms.add_uniform("particle_buffer", &vertex_textures[0], Uniform_Image, i++, shader_gen);
         // TODO: This only works because `count` is the first element of `GLArraysIndirectCommand`. Make this more general.
         gen_uniforms.add_uniform("particle_count", &atomic_count_buffer, Uniform_Atomic, i++, shader_gen);
         gen_uniforms.add_uniform("max_particles", &vertex_count, i++, shader_gen);
@@ -81,20 +82,27 @@ Particles* InitParticles()
     particles->shader_gen = CreateShaderProgram(SHADER_PARTICLE_GEN);
     particles->shader_sim = CreateShaderProgram(SHADER_PARTICLE_SIM);
     BindShaderToUnitQuad(particles->shader_gen);
+    const char* varyings[] = {"out_Position"};
+    glTransformFeedbackVaryings(particles->shader_sim, 1, varyings, GL_INTERLEAVED_ATTRIBS);
     VertexDefBindToShader(particles->particle_vertex_def, particles->shader_sim);
     particles->CaptureUniforms();
 
     particles->vertex_count = 65536;
     unsigned int vertex_len = sizeof(ParticleVert);
-    glGenBuffers(1, &particles->vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, particles->vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, vertex_len * particles->vertex_count, NULL, GL_DYNAMIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glGenTextures(1, &particles->vertex_texture);
-    glBindTexture(GL_TEXTURE_BUFFER, particles->vertex_texture);
-    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, particles->vertex_buffer);
-    glBindBuffer(GL_TEXTURE_BUFFER, 0);
+    glGenBuffers(Particles::BUFFER_COUNT, particles->vertex_buffers);
+    glGenTextures(Particles::BUFFER_COUNT, particles->vertex_textures);
+
+    for(unsigned int i = 0; i < Particles::BUFFER_COUNT; i++)
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, particles->vertex_buffers[i]);
+        glBufferData(GL_ARRAY_BUFFER, vertex_len * particles->vertex_count, NULL, GL_DYNAMIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindTexture(GL_TEXTURE_BUFFER, particles->vertex_textures[i]);
+        glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, particles->vertex_buffers[i]);
+        glBindBuffer(GL_TEXTURE_BUFFER, 0);
+    }
 
     glGenBuffers(1, &particles->atomic_count_buffer);
     glBindBuffer(GL_ATOMIC_COUNTER_BUFFER, particles->atomic_count_buffer);
@@ -113,7 +121,11 @@ Particles* InitParticles()
 
 void DestroyParticles(Particles* particles)
 {
-    DestroyTexture(particles->vertex_buffer);
+    for(unsigned int i = 0; i < Particles::BUFFER_COUNT; i++)
+    {
+        glDeleteTextures(1, &particles->vertex_textures[i]);
+        glDeleteBuffers(1, &particles->vertex_buffers[i]);
+    }
     DestroyProgramAndAttachedShaders(particles->shader_gen);
     DestroyProgramAndAttachedShaders(particles->shader_sim);
 
@@ -144,10 +156,33 @@ void UpdateParticles(Particles* particles)
     particles->sim_uniforms.bind();
 
     glBindBuffer(GL_DRAW_INDIRECT_BUFFER, particles->atomic_count_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, particles->vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, particles->vertex_buffers[0]);
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, particles->vertex_buffers[1]);
     ApplyVertexDef(particles->particle_vertex_def);
+    glBeginTransformFeedback(GL_POINTS);
     glDrawArraysIndirect(GL_POINTS, 0);
+    glEndTransformFeedback();
     ClearVertexDef(particles->particle_vertex_def);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_TRANSFORM_FEEDBACK_BUFFER, 0);
     glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0);
+
+    // Rotate buffers.
+
+    GLuint tTex = particles->vertex_textures[0];
+    GLuint tBuf = particles->vertex_buffers[0];
+
+    for(unsigned int i = 0; i < Particles::BUFFER_COUNT; i++)
+    {
+        if(i < Particles::BUFFER_COUNT - 1)
+        {
+            particles->vertex_textures[i] = particles->vertex_textures[i + 1];
+            particles->vertex_buffers[i] = particles->vertex_buffers[i + 1];
+        }
+        else
+        {
+            particles->vertex_textures[i] = tTex;
+            particles->vertex_buffers[i] = tBuf;
+        }
+    }
 }
